77271595f4468da9191a5dd3a11473b903fd75f4
shelp12138 <zhouyuhang22@huawei.com>
2025-10-22 01:43:00 +0800

    return
    Signed-off-by: shelp12138 <zhouyuhang22@huawei.com>

--- services/bluetooth/ipc/include/bluetooth_host_stub.h
+++ services/bluetooth/ipc/include/bluetooth_host_stub.h
@@ -120,8 +120,6 @@ private:
     int32_t GetCarKeyDfxDataInner(MessageParcel &data, MessageParcel &reply);
     int32_t SetCarKeyCardDataInner(MessageParcel &data, MessageParcel &reply);
     int32_t NotifyDialogResultInner(MessageParcel &data, MessageParcel &reply);
-    int32_t StartRemoteSdpSearchInner(MessageParcel &data, MessageParcel &reply);
-    int32_t GetRemoteServicesInner(MessageParcel &data, MessageParcel &reply);
     static const std::map<uint32_t, std::function<ErrCode(BluetoothHostStub *, MessageParcel &, MessageParcel &)>>
         memberFuncMap_;
 
